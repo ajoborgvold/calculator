eab3e4e85b81f56f85f81e12abaa057753ed4b3a
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-10-09 12:13:40 +0200

    Created new calculator

---
+++ src/components/length/CmToInch.jsx
@@ -0,0 +1,9 @@
+const CmToInch = () => {
+    return (
+        <div className="calculator-wrapper">
+            
+        </div>
+    )
+}
+
+export default CmToInch
--- src/components/length/Length.jsx
+++ src/components/length/Length.jsx
@@ -1,8 +1,9 @@
 const Length = () => {
     return (
-        <main>
-            <h1>Length conversion goes here</h1>
-        </main>
+        <section className="calculators-container">
+            <h1 className="main-heading">Length conversion</h1>
+            <h2 className="sub-heading">Centimetres = inches</h2>
+        </section>
     )
 }
 
