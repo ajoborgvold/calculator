e5a7d4fe752ba3c5a12950329f9317f996c197c6
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-10-23 15:17:11 +0200

    Created conditional rendering of header elements based on window width

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,37 @@
+import { useCallback, useRef, useState, useEffect } from "react"
 import { FiMenu } from "react-icons/fi"
-import { useCallback, useRef, useState } from "react"
 import NavBar from "./NavBar"
 import useClickOutside from "../utils/useClickOutside"
 
 const Header = ({isMenuOpen, setIsMenuOpen, toggleMenu}) => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
     const menuRef = useRef()
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowWidth(window.innerWidth)
+        }
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
 
     const closeMenu = useCallback(() => setIsMenuOpen(false), [])
     useClickOutside(menuRef, closeMenu)
 
-    const headerSmall = (
+    const headerEl = windowWidth < 768 ?
         <div className="header--small">
-            <FiMenu onClick={toggleMenu} className="menu-icon" tabIndex="0"/>
-            {isMenuOpen && <NavBar navClass="header__nav-bar--vertical" menuRef={menuRef}/>}
+            <FiMenu onClick={toggleMenu} className="menu-icon" tabIndex="0" />
+            {isMenuOpen && <NavBar navClass="header__nav-bar--vertical" menuRef={menuRef} />}
         </div>
-    )
-
-    const headerLarge = <NavBar navClass="header__nav-bar--horizontal"/>
+        : <NavBar navClass="header__nav-bar--horizontal"/>
 
     return (
         <header>
-            {headerSmall}
-            {headerLarge}
+            {headerEl}
         </header>
     )
 }
