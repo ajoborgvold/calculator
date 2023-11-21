3bed9d941f284f4c5252206c47c9a63af04ca3cc
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-11-21 15:49:53 +0100

    Working on mobile navbar UI

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,34 +2,37 @@ import { NavLink } from "react-router-dom"
 import { CgClose } from "react-icons/cg"
 
 const NavBar = ({ navClass, isMenuOpen, closeMenu, menuRef }) => {
+    const navLinkClass = navClass === "header__nav-bar--vertical" ? "nav-link--border" : "nav-link--bare"
+
+
     return (
         <>
             <nav className={navClass} ref={menuRef && menuRef}>
                 {isMenuOpen && <CgClose onClick={closeMenu} className="icon close-icon" /> }
                 <NavLink
                     to="/"
-                    className={({ isActive }) => isActive ? 'nav-link nav-link--regular nav-link--active' : 'nav-link nav-link--regular'}
+                    className={({ isActive }) => isActive ? `nav-link ${navLinkClass}--active` : `nav-link ${navLinkClass}`}
                     onClick={closeMenu}
                 >
                     Home
                 </NavLink>
                 <NavLink
                     to="percentage"
-                    className={({ isActive }) => isActive ? 'nav-link nav-link--regular nav-link--active' : 'nav-link nav-link--regular'}
+                    className={({ isActive }) => isActive ? `nav-link ${navLinkClass}--active` : `nav-link ${navLinkClass}`}
                     onClick={closeMenu}
                 >
                     Percentage
                 </NavLink>
                 <NavLink
                     to="unit-conversion"
-                    className={({ isActive }) => isActive ? 'nav-link nav-link--regular nav-link--active' : 'nav-link nav-link--regular'}
+                    className={({ isActive }) => isActive ? `nav-link ${navLinkClass}--active` : `nav-link ${navLinkClass}`}
                     onClick={closeMenu}
                 >
                     Unit conversion
                 </NavLink>
                 <NavLink
                     to="time"
-                    className={({ isActive }) => isActive ? 'nav-link nav-link--regular nav-link--active' : 'nav-link nav-link--regular'}
+                    className={({ isActive }) => isActive ? `nav-link ${navLinkClass}--active` : `nav-link ${navLinkClass}`}
                     onClick={closeMenu}
                 >
                     Time
