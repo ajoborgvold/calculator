519121959ad5b4dc5243d18eced4e0b710244c81
ajo <115482909+ajoborgvold@users.noreply.github.com>
2024-05-13 13:41:23 +0200

    clean up code after footer redesign

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,38 +1,10 @@
-import { FaGithub, FaLinkedinIn } from "react-icons/fa"
-import useGetWindowWidth from "../hooks/useGetWindowWidth"
-
 const Footer = () => {
-  const windowWidth = useGetWindowWidth()
-
-  const footerClass =
-    windowWidth < 768 ? "footer--small-screen" : "footer--large-screen"
-
   const date = new Date()
   const year = date.getFullYear()
 
   return (
-    <footer className={footerClass}>
-      {/* <a
-        href="https://www.linkedin.com/in/ajo-borgvold/"
-        target="_blank"
-        rel="noreferrer"
-        className="a--icon"
-        aria-hidden="false"
-        aria-label="Check out my LinkedIn profile"
-      >
-        <FaLinkedinIn className="icon icon--link" />
-      </a> */}
+    <footer>
       <p>{year} &#169; Ajo Borgvold</p>
-      {/* <a
-        href="https://github.com/ajoborgvold/calculator"
-        target="_blank"
-        rel="noreferrer"
-        className="a--icon"
-        aria-hidden="false"
-        aria-label="Check out the GitHub repo for this app"
-      >
-        <FaGithub className="icon icon--link" />
-      </a> */}
     </footer>
   )
 }
